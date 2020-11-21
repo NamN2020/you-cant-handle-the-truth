4dcfdd3ef613ea04868e1eb1fdc05a9413d2c134
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-21 13:54:01 -0800

    Cleaned up code

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,7 +1,5 @@
 import org.junit.Test;
-
 import java.util.*;
-
 import static org.junit.Assert.assertEquals;
 /*
     Nam Nguyen
@@ -32,7 +30,7 @@ public class HandleTruthTest {
         ArrayList<TreeMap<Integer, Set<String>>> testCase = getTestCases();
 
         for (int i = 0; i < inputs.length; i++) {
-            TreeMap<Integer, String> actual = HandleTruth.wordCount(inputs[i]);
+            TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount(inputs[i]);
             TreeMap<Integer, Set<String>> expected = testCase.get(i);
 
             assertEquals(expected, actual);
@@ -40,11 +38,11 @@ public class HandleTruthTest {
     }
 
     public static ArrayList<TreeMap<Integer, Set<String>>> getTestCases() {
-        ArrayList<TreeMap<Integer, Set<String>>> testCases = new ArrayList<TreeMap<Integer, Set<String>>>();
+        ArrayList<TreeMap<Integer, Set<String>>> testCases = new ArrayList<>();
 
         // TEST CASE 1
-        TreeMap<Integer, Set<String>> testCase1 = new TreeMap<Integer, Set<String>>();
-        Set<String> testCase1oneWord = new HashSet<String>();
+        TreeMap<Integer, Set<String>> testCase1 = new TreeMap<>();
+        Set<String> testCase1oneWord = new HashSet<>();
         testCase1oneWord.add("weinberg");
         testCase1oneWord.add("whos");
         testCase1oneWord.add("be");
@@ -73,87 +71,86 @@ public class HandleTruthTest {
         testCase1oneWord.add("to");
         testCase1.put(1, testCase1oneWord);
 
-        Set<String> testCase1twoWords = new HashSet<String>();
+        Set<String> testCase1twoWords = new HashSet<>();
         testCase1twoWords.add("a");
         testCase1twoWords.add("walls");
         testCase1twoWords.add("have");
         testCase1.put(2, testCase1twoWords);
 
-        Set<String> testCase1threeWords = new HashSet<String>();
+        Set<String> testCase1threeWords = new HashSet<>();
         testCase1threeWords.add("you");
         testCase1.put(3, testCase1threeWords);
 
         //  TEST CASE 2
-        TreeMap<Integer, Set<String>> testCase2 = new TreeMap<Integer, Set<String>>();
-        Set<String> testCase2oneWord = new HashSet<String>();
+        TreeMap<Integer, Set<String>> testCase2 = new TreeMap<>();
+        Set<String> testCase2oneWord = new HashSet<>();
         testCase2oneWord.add("what");
         testCase2oneWord.add("think");
         testCase2oneWord.add("become");
         testCase2.put(1, testCase2oneWord);
 
-        Set<String> testCase2twoWords = new HashSet<String>();
+        Set<String> testCase2twoWords = new HashSet<>();
         testCase2twoWords.add("we");
         testCase2.put(2, testCase2twoWords);
 
-
         //  TEST CASE 3
-        TreeMap<Integer, Set<String>> testCase3 = new TreeMap<Integer, Set<String>>();
-        Set<String> testCase3oneWord = new HashSet<String>();
+        TreeMap<Integer, Set<String>> testCase3 = new TreeMap<>();
+        Set<String> testCase3oneWord = new HashSet<>();
         testCase3oneWord.add("whatever");
         testCase3oneWord.add("you");
         testCase3oneWord.add("it");
         testCase3oneWord.add("well");
         testCase3.put(1, testCase3oneWord);
 
-        Set<String> testCase3twoWords = new HashSet<String>();
+        Set<String> testCase3twoWords = new HashSet<>();
         testCase3twoWords.add("do");
         testCase3.put(2, testCase3twoWords);
 
         //  TEST CASE 4
-        TreeMap<Integer, Set<String>> testCase4 = new TreeMap<Integer, Set<String>>();
-        Set<String> testCase4oneWord = new HashSet<String>();
+        TreeMap<Integer, Set<String>> testCase4 = new TreeMap<>();
+        Set<String> testCase4oneWord = new HashSet<>();
         testCase4oneWord.add("one");
         testCase4.put(1, testCase4oneWord);
 
-        Set<String> testCase4twoWords = new HashSet<String>();
+        Set<String> testCase4twoWords = new HashSet<>();
         testCase4twoWords.add("two");
         testCase4.put(2, testCase4twoWords);
 
-        Set<String> testCase4threeWords = new HashSet<String>();
+        Set<String> testCase4threeWords = new HashSet<>();
         testCase4threeWords.add("three");
         testCase4.put(3, testCase4threeWords);
 
-        Set<String> testCase4fourWords = new HashSet<String>();
+        Set<String> testCase4fourWords = new HashSet<>();
         testCase4fourWords.add("four");
         testCase4.put(4, testCase4fourWords);
 
-        Set<String> testCase4fiveWords = new HashSet<String>();
+        Set<String> testCase4fiveWords = new HashSet<>();
         testCase4fiveWords.add("five");
         testCase4.put(5, testCase4fiveWords);
 
-        Set<String> testCase4sixWords= new HashSet<String>();
+        Set<String> testCase4sixWords= new HashSet<>();
         testCase4sixWords.add("six");
         testCase4.put(6, testCase4sixWords);
 
-        Set<String> testCase4sevenWords = new HashSet<String>();
+        Set<String> testCase4sevenWords = new HashSet<>();
         testCase4sevenWords.add("seven");
         testCase4.put(7, testCase4sevenWords);
 
-        Set<String> testCase4eightWords = new HashSet<String>();
+        Set<String> testCase4eightWords = new HashSet<>();
         testCase4eightWords.add("eight");
         testCase4.put(8, testCase4eightWords);
 
-        Set<String> testCase4nineWords = new HashSet<String>();
+        Set<String> testCase4nineWords = new HashSet<>();
         testCase4nineWords.add("nine");
         testCase4.put(9, testCase4nineWords);
 
-        Set<String> testCase4tenWords = new HashSet<String>();
+        Set<String> testCase4tenWords = new HashSet<>();
         testCase4tenWords.add("ten");
         testCase4.put(10, testCase4tenWords);
 
         //  TEST CASE 5
-        TreeMap<Integer, Set<String>> testCase5 = new TreeMap<Integer, Set<String>>();
-        Set<String> testCase5oneWord = new HashSet<String>();
+        TreeMap<Integer, Set<String>> testCase5 = new TreeMap<>();
+        Set<String> testCase5oneWord = new HashSet<>();
         testCase5oneWord.add("head");
         testCase5oneWord.add("feet");
         testCase5oneWord.add("can");
@@ -166,19 +163,19 @@ public class HandleTruthTest {
         testCase5oneWord.add("direction");
         testCase5.put(1, testCase5oneWord);
 
-        Set<String> testCase5twoWords = new HashSet<String>();
+        Set<String> testCase5twoWords = new HashSet<>();
         testCase5twoWords.add("in");
         testCase5twoWords.add("have");
         testCase5twoWords.add("your");
         testCase5.put(2, testCase5twoWords);
 
-        Set<String> testCase5fourWords = new HashSet<String>();
+        Set<String> testCase5fourWords = new HashSet<>();
         testCase5fourWords.add("you");
         testCase5.put(4, testCase5fourWords);
 
         //  TEST CASE 6
-        TreeMap<Integer, Set<String>> testCase6 = new TreeMap<Integer, Set<String>>();
-        Set<String> testCase6empty = new HashSet<String>();
+        TreeMap<Integer, Set<String>> testCase6 = new TreeMap<>();
+        Set<String> testCase6empty = new HashSet<>();
         testCase6empty.add("");
         testCase6.put(1, testCase6empty);
 
