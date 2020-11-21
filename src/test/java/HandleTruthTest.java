e474f174fd4f9062785d580830db93ab53ffa7ad
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-21 02:22:27 -0800

    Test Working (Adding more Cases)

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -21,39 +21,87 @@ public class HandleTruthTest {
     @Test
     public void testWordCount() {
         String[] inputs = {
-                "Four Three",
-                // {"son we live in a world that has walls and those walls have to be guarded by men with guns " +
-                // "whos gonna do it you you lieutenant weinberg i have a greater responsibility than you " +
-                // "can possibly fathom you weep for santiago and you curse the marines you have that luxury " +
-                // "you have the luxury of not knowing what i know that santiagos death while tragic probably" +
-                // " saved lives and my existence while grotesque and incomprehensible to you saves lives"}
+
+                "One Two Two",
+                "son we live in a world that has walls and those walls have to be guarded by men with guns " +
+                        "whos gonna do it you you lieutenant weinberg i have a greater responsibility than you"
         };
 
 
-        TreeMap<Integer, Set<String>> testMap = new TreeMap<Integer, Set<String>>();
-        Set<String> oneWord = new HashSet<String>();
-        oneWord.add("Four");
-        oneWord.add("Three");
-        testMap.put(1, oneWord);
+        ArrayList<TreeMap<Integer, Set<String>>> testCase = getTestCases();
 
-        TreeMap<Integer, Set<String>> testMap2 = new TreeMap<Integer, Set<String>>();
-        Set<String> oneWord2 = new HashSet<String>();
-
-        //TreeMap<Integer, Set<String>>[] expected = {testMap};
-
-        ArrayList<TreeMap<Integer, Set<String>>> arrayTing = new ArrayList<TreeMap<Integer, Set<String>>>();
-        arrayTing.add(testMap);
-
-        TreeMap<Integer, Set<String>> temp = HandleTruth.wordCount(inputs[0]);
-
-        assertEquals(testMap, temp);
 
         for (int i = 0; i < inputs.length; i++) {
-            TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount(inputs[i]);
-            TreeMap<Integer, Set<String>> expected = arrayTing.get(i);
+            TreeMap<Integer, String> actual = HandleTruth.wordCount(inputs[i]);
+            TreeMap<Integer, Set<String>> expected = testCase.get(i);
 
             assertEquals(expected, actual);
         }
+    }
 
+    public static ArrayList<TreeMap<Integer, Set<String>>> getTestCases() {
+        ArrayList<TreeMap<Integer, Set<String>>> testCases = new ArrayList<TreeMap<Integer, Set<String>>>();
+
+        TreeMap<Integer, Set<String>> testCase1 = new TreeMap<Integer, Set<String>>();
+        Set<String> testCase1oneWord = new HashSet<String>();
+        testCase1oneWord.add("One");
+        testCase1.put(1, testCase1oneWord);
+        Set<String> testCase1twoWords = new HashSet<String>();
+        testCase1twoWords.add("Two");
+        testCase1.put(2, testCase1twoWords);
+
+        TreeMap<Integer, Set<String>> testCase2 = new TreeMap<Integer, Set<String>>();
+        Set<String> testCase2oneWord = new HashSet<String>();
+        testCase2oneWord.add("weinberg");
+        testCase2oneWord.add("whos");
+        testCase2oneWord.add("be");
+        testCase2oneWord.add("guns");
+        testCase2oneWord.add("lieutenant");
+        testCase2oneWord.add("do");
+        testCase2oneWord.add("we");
+        testCase2oneWord.add("that");
+        testCase2oneWord.add("guarded");
+        testCase2oneWord.add("son");
+        testCase2oneWord.add("world");
+        testCase2oneWord.add("responsibility");
+        testCase2oneWord.add("and");
+        testCase2oneWord.add("by");
+        testCase2oneWord.add("men");
+        testCase2oneWord.add("than");
+        testCase2oneWord.add("has");
+        testCase2oneWord.add("greater");
+        testCase2oneWord.add("live");
+        testCase2oneWord.add("those");
+        testCase2oneWord.add("in");
+        testCase2oneWord.add("gonna");
+        testCase2oneWord.add("i");
+        testCase2oneWord.add("it");
+        testCase2oneWord.add("with");
+        testCase2oneWord.add("to");
+        testCase2.put(1, testCase2oneWord);
+
+        Set<String> testCase2twoWord = new HashSet<String>();
+        testCase2twoWord.add("a");
+        testCase2twoWord.add("walls");
+        testCase2twoWord.add("have");
+        testCase2.put(2, testCase2twoWord);
+
+        Set<String> testCase2threeWord = new HashSet<String>();
+        testCase2threeWord.add("you");
+        testCase2.put(3, testCase2threeWord);
+
+        TreeMap<Integer, Set<String>> testCase3 = new TreeMap<Integer, Set<String>>();
+        Set<String> testCase3Words = new HashSet<String>();
+
+        TreeMap<Integer, Set<String>> testCase4 = new TreeMap<Integer, Set<String>>();
+        Set<String> testCase4Words = new HashSet<String>();
+
+        TreeMap<Integer, Set<String>> testCase5 = new TreeMap<Integer, Set<String>>();
+        Set<String> testCase5Words = new HashSet<String>();
+
+
+        testCases.add(testCase1);
+        testCases.add(testCase2);
+        return testCases;
     }
 }
